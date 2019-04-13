51284891ffb5e6d3220c80644f2a67968f27cf50
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-04-13 23:30:38 +0100

    Update and rename installed.conf to installed.bash

---
+++ repositories/installed.bash
@@ -0,0 +1,5 @@
+[-d /bedrock] && location="/bedrock/strat/exherbo/var/db/paludis/repositories/installed" || location="location="/var/db/paludis/repositories/installed"
+format=exndbam
+[-d /bedrock] && names_cache="/bedrock/strat/exherbo/var/cache/paludis/names" || name_cache="/var/cache/paludis/names"
+split_debug_location="/usr/x86_64-pc-linux-gnu/lib/debug"
+tool_prefix="x86_64-pc-linux-gnu-"
