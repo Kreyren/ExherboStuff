ad62270d8d1601b48fbcc851f1bb4795c4cb7f76
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-04-13 23:34:21 +0100

    Update and rename unwritten.conf to unwritten.bash

---
+++ repositories/unwritten.bash
@@ -0,0 +1,4 @@
+format=unwritten
+[-d /bedrock] && location="/bedrock/strat/exherbo/var/db/paludis/repositories/unwritten" || location="/var/db/paludis/repositories/unwritten"
+sync="git+https://git.exherbo.org/git/unwritten.git"
+importance="-100"
