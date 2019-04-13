10ea005000f359f41bec16ae27b81421217ecbfe
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-04-13 23:31:02 +0100

    Update installed.bash

--- repositories/installed.bash
+++ repositories/installed.bash
@@ -1,4 +1,4 @@
-[-d /bedrock] && location="/bedrock/strat/exherbo/var/db/paludis/repositories/installed" || location="location="/var/db/paludis/repositories/installed"
+[-d /bedrock] && location="/bedrock/strat/exherbo/var/db/paludis/repositories/installed" || location="/var/db/paludis/repositories/installed"
 format=exndbam
 [-d /bedrock] && names_cache="/bedrock/strat/exherbo/var/cache/paludis/names" || name_cache="/var/cache/paludis/names"
 split_debug_location="/usr/x86_64-pc-linux-gnu/lib/debug"
