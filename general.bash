b94e8e8b50fef43bb98c1f1f4a75f1eaa1d32aec
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-04-14 02:33:45 +0100

    Update general.bash

--- general.bash
+++ general.bash
@@ -1,1 +1,4 @@
-[-d /bedrock ] && world="/bedrock/strat/exherbo/" || world="/"
+[-d /bedrock] && world="/bedrock/strat/exherbo/" || world="/"
+# reduced_username="kreyren"
+# distribution="" # If set, overrides the distribution used when distribution-specific defaults are used. -> What?
+[-d /bedrock] && root="/bedrock/strat/exherbo/ || root="/"
