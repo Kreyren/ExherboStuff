0898c81ba811ba89f82af16f37dd2ba560aa632b
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-04-14 02:04:55 +0100

    Update 00-kreyren.bash

--- repositories/00-kreyren.bash
+++ repositories/00-kreyren.bash
@@ -1,4 +1,4 @@
 [-d /bedrock] && location="/bedrock/strat/exherbo/etc/paludis" || location="/etc/paludis"
 sync="git@github.com:Kreyren/ExherboStuff.git"
-priority="-999"
+priority="9999" # Always higgest -> Load first
 format="e" # Sane?
