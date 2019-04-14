5ba427c80ec87b3100b2c905387f1dd13e47b0b5
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-04-14 02:03:36 +0100

    Update arbor.bash

--- repositories/arbor.bash
+++ repositories/arbor.bash
@@ -1,6 +1,6 @@
-[-d /bedrock] && location="/bedrock/strat/exherbo/var/db/paludis/repositories/arbor" || location = ${root}/var/db/paludis/repositories/arbor
+[-d /bedrock] && location="/bedrock/strat/exherbo/var/db/paludis/repositories/arbor" || location="/var/db/paludis/repositories/arbor"
 sync="git+https://git.exherbo.org/git/arbor.git"
 profiles=${location}/profiles/amd64
 format="e"
-[-d /bedrock] && name_cache="/bedrock/strat/exherbo/var/cache/paludis/names" || names_cache=${root}/var/cache/paludis/names
-[-d /bedrock] && write_cache="/bedrock/strat/exherbo/var/cache/paludis/metadata" || write_cache = ${root}/var/cache/paludis/metadata
+[-d /bedrock] && name_cache="/bedrock/strat/exherbo/var/cache/paludis/names" || names_cache="/var/cache/paludis/names"
+[-d /bedrock] && write_cache="/bedrock/strat/exherbo/var/cache/paludis/metadata" || write_cache="/var/cache/paludis/metadata"
