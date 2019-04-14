44dc1158d1b99124b6b9b0b4f50ccab31179261d
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-04-14 03:31:15 +0100

    Update options.bash

--- options.bash
+++ options.bash
@@ -1,12 +1,13 @@
-# SYNTAX: category/package: flag=value || {-,}flag
-*/* -* # fuck profile!
+## SYNTAX.conf: category/package: flag=value || {-,}flag
+## SYNTAX.bash: {<,>,=,<=,=>}category/package-version {-,}useflag BUILD=OPTIONS="-j8"
 
-*/* vulkan threads
+*/* -* vulkan threads
+# fuck profile!
 # vulkan - enable vulkan for everything, sane?
 # threads - Allows using all CPU threads for everything, sane?
 
-*/* build_options: jobs=8
+*/* BUILD_OPTIONS="-j8"
 
-*/* targets: -* x86_64-pc-linux-gnu # TODO: cherrypick 32-bit 
+*/* VIDEO_CARDS="-* amdgpu radeonsi"
 
-*/* VIDEO_CARDS: -* amdgpu radeonsi
+*/* TARGETS="-* x86_64-pc-linux-gnu" # TODO: cherrypick 32-bit
